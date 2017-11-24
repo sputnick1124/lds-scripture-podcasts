d6d885d0f7fb02152ee30dc5ed894d09623e3797
Nicklas Stockton <sputnick1124@comcast.net>
2017-11-24 09:50:22 -0500

    directory change in cron script

--- update-bofm.sh
+++ update-bofm.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+pushd "$DIR"
 ./download-segment.sh bofm
 git add bofm/*.mp3
 git add rss/bofm.rss
 git commit -m "daily update of bofm"
 git push
+popd "$DIR"
