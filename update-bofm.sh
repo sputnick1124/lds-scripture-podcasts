ac17584de1900ecc4edb8167dd7154555590f912
Nicklas O Stockton <stocktno@ucfsh.ucfilespace.uc.edu>
2017-11-29 02:31:24 -0500

    get rid of non-existent mp3 git add

--- update-bofm.sh
+++ update-bofm.sh
@@ -2,7 +2,6 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 pushd "$DIR"
 ./download-segment.sh bofm
-git add bofm/*.mp3
 git add rss/bofm.rss
 git commit -m "daily update of bofm"
 git push
